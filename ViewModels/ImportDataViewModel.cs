0c9880884eb4fc3ea4dfd684e7773904d3fb27dc
Azkee <83642891+Martufa@users.noreply.github.com>
2025-05-15 17:11:16 +0200

    fix: fully implement loading from csv, no crashes, no exceptions, it just works

--- ViewModels/ImportDataViewModel.cs
+++ ViewModels/ImportDataViewModel.cs
@@ -14,6 +14,7 @@ using System.Windows;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using System.Collections.Concurrent;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
 
 namespace JuanLog.ViewModels
@@ -54,75 +55,81 @@ namespace JuanLog.ViewModels
 
         private async void ProcessCSV(string fileName)
         {
-            string[] lines = File.ReadAllLines(System.IO.Path.ChangeExtension(fileName, ".csv"));
-            var db = new JuanLogDBContext();
-            // List<Exercise> allExercises = await Exercise.GetAllExercises();
-            ConcurrentDictionary<string, Exercise> allExercises = new();
-            foreach (Exercise e in await Exercise.GetAllExercises())
-            {
-                allExercises.TryAdd(e.ExerciseName, e);
-            }
+            try { 
+                string[] lines = File.ReadAllLines(System.IO.Path.ChangeExtension(fileName, ".csv"));
+                var db = new JuanLogDBContext();
 
-            foreach (string line in lines)
-            {
-                string[] data = line.Split(';');
-                if (data.Length == 1) // Juan ten den necvičil
+                ConcurrentDictionary<string, Exercise> allExercises = new();
+                foreach (Exercise e in await Exercise.GetAllExercises())
                 {
-                    continue;
+                    allExercises.TryAdd(e.ExerciseName, e);
                 }
-                int weight = 0;
-                DateTime date = DateTime.Now;
-                try
+
+                foreach (string line in lines)
                 {
+                    string[] data = line.Split(';');
+                    if (data.Length == 1) // Juan did not exercise
+                    {
+                        continue;
+                    }
+                    int weight = 0;
+                    DateTime date = DateTime.Parse(data[0]);
                     weight = int.Parse(data[2]);
-                    date = DateTime.Parse(data[0]);
-                } catch
-                {
-                    // MessageBox.Show("Něco z toho je špatně ve dni: " + data[0]);
-                    // weight vyhodí chybu, juan ten den necvičil
-                }
                 
-                
-                for (int i = 0; i < 5; i++)
-                {
-                    string exerciseName = data[6 * i + 1];
-                    if (! allExercises.ContainsKey(exerciseName))
-                    {
-                        var addition = new Exercise { ExerciseName = exerciseName, CategoryId = 1 };
-                        try
-                        {   
-                            allExercises.TryAdd(exerciseName, addition);
-                            db.Exercises.Add(addition);
 
-                            await db.SaveChangesAsync();
-                        } catch (Microsoft.EntityFrameworkCore.DbUpdateException)
+                    for (int i = 0; i < 5; i++)
+                    {
+                        string exerciseName = data[6 * i + 1];
+                        if (exerciseName == "")
                         {
-                            // this exception is thrown if two same keys are being inserted by different threads:
-                            // the primary keys cannot allow for duplicities
-                            // should this error occur, we can just insert the exercise as-is, without creating new category
-                            // we just need to remove it from the log
-                            db.Exercises.Entry(addition).State = EntityState.Detached;
+                            break;
                         }
-                    }
-                    ExerciseEntry entry = new ExerciseEntry { UserId = ActiveUser.Id, ExerciseName = exerciseName, Weight = weight, When = date };
-                    var addedEntry = await db.ExerciseEntries.AddAsync(entry);
-                    await db.SaveChangesAsync();
-
-                    var entryId = addedEntry.Property(x => x.EntryId).CurrentValue;
-                    for (int rep = 0; rep < 4; rep++)
-                    {
-                        try
+                    
+                        // if the exercise doesn't exist, add it
+                        if (! allExercises.ContainsKey(exerciseName))
                         {
-                            int currentReps = int.Parse(data[6 * i + 1 + rep]);
+                            var addition = new Exercise { ExerciseName = exerciseName, CategoryId = 1 };
+                            try
+                            {   
+                                allExercises.TryAdd(exerciseName, addition);
+                                db.Exercises.Add(addition);
+
+                                await db.SaveChangesAsync();
+                            } catch (Microsoft.EntityFrameworkCore.DbUpdateException)
+                            {
+                                // this exception is thrown if two same keys are being inserted by different threads:
+                                // the primary keys cannot allow for duplicities
+                                // should this error occur, we can just insert the exercise as-is, without creating new category
+                                // we just need to remove it from the log
+                                db.Exercises.Entry(addition).State = EntityState.Detached;
+                            }
+                        }
+                    
+                        ExerciseEntry entry = new ExerciseEntry { UserId = ActiveUser.Id, ExerciseName = exerciseName, Weight = weight, When = date };
+                        var addedEntry = await db.ExerciseEntries.AddAsync(entry);
+                        await db.SaveChangesAsync();
+
+                        var entryId = addedEntry.Property(x => x.EntryId).CurrentValue;
+                        for (int rep = 1; rep <= 5; rep++)
+                        {
+                            string repString = data[6 * i + 1 + rep];
+                            if (repString == "")
+                            {
+                                break;
+                            }
+                            int currentReps = (int)(float.Parse(repString, CultureInfo.InvariantCulture));
                             db.SetTable.Add(new Set { EntryId = entryId, Repetitions = currentReps });
+
                         }
-                        catch {
-                            // MessageBox.Show("Zapsání setu " + date + " se nepodařilo...promiň, Juane.");
-                        }
+                        await db.SaveChangesAsync();
+
+
                     }
-
-
                 }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Načítání z csv souboru selhalo. Zkontroluj prosím formát souboru.");
             }
         }
     }
